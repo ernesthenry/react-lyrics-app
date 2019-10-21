2360236bebc29ed2b9c1af87cc6a05437e561d33
ernesthenry <henry38ernest@gmail.com>
2019-10-21 12:45:09 -0400

    Add more tracks that will be displayed to the user

--- src/context.js
+++ src/context.js
@@ -6,12 +6,12 @@ const Context = React.createContext();
 export class Provider extends Component {
     state = {
         track_list : [],
-        heading: 'Top 20 Tracks'
+        heading: 'Top 100 Tracks'
     }
 
     componentDidMount(){
         axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?
-        chart_name=top&page=1&page_size=20&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+        chart_name=top&page=1&page_size=100&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
         .then(
         res => {
         console.log(res.data);
